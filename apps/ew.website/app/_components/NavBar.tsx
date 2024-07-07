7ab330ec8b7cc1b650d35228a9c925c86bdd2b6c
Ed Wong <edmondwong6279@gmail.com>
2024-07-07 17:22:12 +0100

    hamburger wip

--- apps/ew.website/app/_components/NavBar.tsx
+++ apps/ew.website/app/_components/NavBar.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Hamburger from "hamburger-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState, useContext, useEffect } from "react";
-import { ThemeContext } from "../_context";
-import { useWindowSize } from "../_hooks";
+import { useState, useContext, useEffect, useMemo } from "react";
+
+import { ThemeContext } from "@/context";
+import { useWindowSize } from "@/hooks";
 
 export default function NavBar({
   navItems,
@@ -15,9 +17,8 @@ export default function NavBar({
   const path = usePathname();
   const [scrolled, setScrolled] = useState(false);
   const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
-  // TODO add light mode
-  const pathname = usePathname();
   const size = useWindowSize();
+  // TODO add light mode
   const { theme } = useContext(ThemeContext);
 
   const [viewIsMobile, setviewIsMobile] = useState(size.width < mdBreakpoint);
@@ -55,18 +56,41 @@ export default function NavBar({
     }
   }
 
+  const activeNavItem = useMemo(() => {
+    return navItems.filter(({ link }) =>
+      link !== "/" ? path.startsWith(link) : path === link
+    )[0];
+  }, [navItems, path]);
+
   return (
     <nav
       className={`flex justify-center sticky top-0 w-full py-5 px-10 z-50 transition-all ${scrolled ? "bg-[#0a0a0a]" : "bg-transparent"}`}
     >
-      <ul className="justify-between md:flex hidden w-full max-w-screen-xl ">
-        {navItems.map((item, idx) => (
+      {viewIsMobile ? (
+        <div className="flex justify-between items-center w-full">
+          {/* TODO do I want to make this a link? */}
+          <h1
+            className={`bg-start animate-bg-spin bg-[length:200px_100px] text-2xl tracking-widest transition-all bg-clip-text bg-gradient-to-r from-[#F77373] to-[#A193F5] text-transparent font-bold ${mobileMenuIsOpen ? "opacity-0" : "opacity-100"}`}
+          >
+            {activeNavItem?.title || navItems[0].title}
+          </h1>
+          <Hamburger
+            toggled={mobileMenuIsOpen}
+            onToggle={setMobileMenuIsOpen}
+            color="linear-gradient(to right, #F77373, #A193F5"
+          />
+        </div>
+      ) : null}
+      <ul
+        className={`justify-between md:flex w-full max-w-screen-xl min-h-fit transition-all ${mobileMenuIsOpen ? "flex flex-col min-h-screen" : "opacity-0 min-h-0"}`}
+      >
+        {navItems.map(({ link, title }, idx) => (
           <li className="relative" key={`${idx}`}>
             <Link
-              className={`bg-start animate-bg-spin bg-[length:200px_100px] text-2xl tracking-widest font-medium transition-all bg-clip-text after:transition-all after:rounded-md after:content-[''] after:absolute after:-bottom-2 after:w-0 after:left-1/2 after:h-1 hover:after:w-full hover:after:left-0 bg-gradient-to-r from-[#F77373] to-[#A193F5] ${(item.link !== "/" ? path.startsWith(item.link) : path === item.link) ? "text-transparent font-bold after:bg-gradient-to-r after:from-[#F77373] after:to-[#A193F5]" : "text-off-white after:bg-white"}`}
-              href={item.link}
+              className={`bg-start animate-bg-spin bg-[length:200px_100px] text-2xl tracking-widest font-medium transition-all bg-clip-text after:transition-all after:rounded-md after:content-[''] after:absolute after:-bottom-2 after:w-0 after:left-1/2 after:h-1 hover:after:w-full hover:after:left-0 bg-gradient-to-r from-[#F77373] to-[#A193F5] ${(link !== "/" ? path.startsWith(link) : path === link) ? "text-transparent font-bold after:bg-gradient-to-r after:from-[#F77373] after:to-[#A193F5]" : "text-off-white after:bg-white"}`}
+              href={link}
             >
-              {item.title}
+              {title}
             </Link>
           </li>
         ))}
